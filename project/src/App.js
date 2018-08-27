7729eae3fd4b96165a688b2b7b225bb9f0f795cd
Divya <divya_agarwal@trimble.com>
2018-08-27 12:59:34 +0530

    reverting changes

--- project/src/App.js
+++ project/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Manage Customers</h1>
+          <h1 className="App-title">Manage Customer</h1>
         </header>
       </div>
     );
