1f098d4cb2cff5abdfbaf79ec57c2809a38e7d45
dagarwal06 <div11.agarwal@gmail.com>
2018-08-29 14:51:56 +0530

    Implementation of redux store for app.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import {Provider} from 'react-redux';
 import Root from "./components/root/rootContainer"
 import "./App.css";
+
+import appStore  from './store/appStore';
+
+const store = appStore();
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
        <Root />
       </div>
+      </Provider>
     );
   }
 }
--- project/src/store/appStore.js
+++ project/src/store/appStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
-import rootReducer from '../reducers';
+import rootReducer from '../reducers/combineReducer';
 
 export default () => {
   const middleWares = applyMiddleware(thunk,logger);
