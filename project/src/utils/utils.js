497e0d5ee389cd0fbca051c1da53e57e124eed57
dagarwal06 <div11.agarwal@gmail.com>
2018-08-30 12:56:46 +0530

    commiting utils.js for createReducer function

--- project/src/utils/utils.js
+++ project/src/utils/utils.js
@@ -1,7 +1,14 @@
-// All utility functions to be included in this file
 export function createConstants(...constants) {
-    return constants.reduce((acc, constant) => {
-        acc[constant] = constant;
-        return acc;
-    }, {});
+  return constants.reduce((acc, constant) => {
+    acc[constant] = constant;
+    return acc;
+  }, {});
+}
+
+export function createReducer(initialState, reducerMap) {
+  return (state = initialState, action = {}) => {
+    const reducer = reducerMap[action.type];
+
+    return reducer ? reducer(state, action.payload) : state;
+  };
 }
