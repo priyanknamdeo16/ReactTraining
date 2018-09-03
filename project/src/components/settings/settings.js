884d0a2fade15579679186027df2dff7335fa888
plachuriya1990 <plachuriya@gmail.com>
2018-09-03 15:30:10 +0530

    Setting list component

--- project/src/components/settings/settings.js
+++ project/src/components/settings/settings.js
@@ -1,15 +1,38 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as SettingsActionCreator from '../../actions/tabActions';
+import styled from 'styled-components';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-class SettingsComponent extends Component{
-    componentDidMount(){
+const Wrapper = styled.div`
+width: '100%';
+overflowX: 'auto';
+`;
+
+class SettingsComponent extends Component {
+    componentDidMount() {
         this.props.tabIndexChnage.tabChange(1);
     }
-    render(){
+    render() {
         return (
-            <div>settings component here</div>
+            <Wrapper>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Announcement Name</TableCell>
+                        <TableCell>Announcement Start</TableCell>
+                        <TableCell>Announcement End</TableCell>
+                        <TableCell>Status</TableCell>
+                        <TableCell>Downtime Start</TableCell>
+                        <TableCell>Downtime End</TableCell>
+                    </TableRow>
+                </TableHead>
+            </Wrapper>
         )
     }
 }
